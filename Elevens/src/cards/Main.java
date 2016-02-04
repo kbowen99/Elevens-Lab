74a4ece08ae16f8cfadec20dac5e8e644e16b8a7
kbowen99 <kurtisbowen99@gmail.com>
2016-02-04 09:56:27 -0600

    A Start on Part 2

--- Elevens/src/cards/Main.java
+++ Elevens/src/cards/Main.java
@@ -8,16 +8,17 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		CardTester unitTest = new CardTester();
-		unitTest.unitTest("127", "Magic", 9001);
-		unitTest.unitTest("King", "Nathan", -1);
-		unitTest.unitTest("Hettel", "Engineering", 0);
-		unitTest.unitTest("Miller", "Nothing", 3);
+		//unitTest.unitTest("127", "Magic", 9001);
+		//unitTest.unitTest("King", "Nathan", -1);
+		//unitTest.unitTest("Hettel", "Engineering", 0);
+		//unitTest.unitTest("Miller", "Nothing", 3);
 		Win();
 	}
 	
 	public static void Win(){
 		try {
-			Desktop.getDesktop().browse(new URI("https://www.youtube.com/embed/4gd-PY_ammY?rel=0&amp;controls=0&amp;showinfo=0&autoplay=1"));
+			//https://youtu.be/btPJPFnesV4
+			Desktop.getDesktop().browse(new URI("https://youtu.be/btPJPFnesV4"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
